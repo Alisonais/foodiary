4ad8843185c323bf6eaea57518cfa1914ec68b42
AlisonAis <alisonais@ymail.com>
2025-07-13 13:58:55 -0300

    feat: send and receive message in sqs

--- src/application/useCases/meals/MealUploadedUseCase.ts
+++ src/application/useCases/meals/MealUploadedUseCase.ts
@@ -2,6 +2,7 @@ import { Meal } from '@aplication/entities/Meal';
 import { ResourceNotFound } from '@aplication/errors/application/ResourceNotFound';
 import { MealRepository } from '@infra/database/dynamo/repositories/MealRepository';
 import { MealsFileStorgeGateway } from '@infra/gateways/MealsFileStorgeGateway';
+import { MealsQueueGateway } from '@infra/gateways/MealsQueueGateway';
 import { Injectable } from '@kernel/decorators/injectable';
 
 @Injectable()
@@ -10,6 +11,7 @@ export class MealUploadedUseCase {
   constructor(
     private readonly mealRepository: MealRepository,
     private readonly mealsFileStorgeGateway: MealsFileStorgeGateway,
+    private readonly mealsQueueGateway: MealsQueueGateway,
   ) { }
 
   async execute({ fileKey }: MealUploadedUseCase.Input): Promise<MealUploadedUseCase.Output> {
@@ -30,8 +32,7 @@ export class MealUploadedUseCase {
     meal.status = Meal.Status.QUEUED;
 
     await this.mealRepository.save(meal);
-
-    console.log(JSON.stringify(meal, null, 2));
+    await this.mealsQueueGateway.publish({ accountId, mealId });
   }
 }
 export namespace MealUploadedUseCase {
